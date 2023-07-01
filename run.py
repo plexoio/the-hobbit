5258320d21002001ad5726be88c088dede9e81b5
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-07-01 18:46:47 +0000

    understand ROUTIN & VIEW t render files

--- run.py
+++ run.py
@@ -1,14 +1,21 @@
 import os
-from flask import Flask  # first import Class from module
+from flask import Flask, render_template  # first import Class from module
 
 # instance of the class passing __name__ (provides current module or script)
 app = Flask(__name__)
 
 
-@app.route("/")  # help direct the files to Flask
-def index():
-    return "Hello, world!"
+@app.route("/")  # ROUTE help direct the files to Flask | @(decorator)
+def index(): # VIEW
+    return render_template('index.html') # imported method from flask
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 # This condition ensures that the subsequent
 # code block is only executed when run.py is the main entry point.
